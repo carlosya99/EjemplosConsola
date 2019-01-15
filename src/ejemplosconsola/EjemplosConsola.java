2a0cb449a4fd163f591df3993c92204537aff634
Carlos Zea <Carlos Zea@DESKTOP-UILG6L8>
2019-01-15 12:54:28 +0100

    Con el ejercicio 1 empezado solo funciona para calcular el max. pero no para el segundo max.

---
+++ src/ejemplosconsola/EjemplosConsola.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplosconsola;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author Carlos Zea
+ */
+public class EjemplosConsola {
+    
+    //declaro u a array de ints de instancia
+    int[] listaNumeros	= {20, 31, 27, 2, 5, 99};
+    
+    /*
+    el metodo maximos va aclacular el máximo y el segndo máximo de una lista de números
+    */
+    private int[] maximos (int[] lista){
+       //declaramos un array de dos elementos para guardar el máximo y el segundo máximo
+        int[] listaMaximos = {lista[0], lista[0]};
+        for (int i=0; i< lista.length; i++){
+            if (listaMaximos[0] < lista[i]){
+                if (listaMaximos[1] == listaMaximos[0]){
+                    listaMaximos[1] = listaMaximos[0];
+                            
+                }
+                listaMaximos[0] = lista[i];
+            }
+        }
+        return listaMaximos;
+    }
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        EjemplosConsola ejercicios = new EjemplosConsola();
+           
+        
+           System.out.println( Arrays.toString( ejercicios.maximos(ejercicios.listaNumeros)));
+    }
+    
+}
