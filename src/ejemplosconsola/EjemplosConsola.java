b61605df74add9a2a96b98787c58e644359e6035
Carlos Zea <Carlos Zea@DESKTOP-UILG6L8>
2019-02-05 12:25:20 +0100

    Anagrama -> Resuelto 100% a la primera

--- src/ejemplosconsola/EjemplosConsola.java
+++ src/ejemplosconsola/EjemplosConsola.java
@@ -9,63 +9,67 @@ import java.util.Arrays;
 
 /**
  *
- * @author Carlos Zea
+ * @author xp
  */
 public class EjemplosConsola {
 
-    //declaro u a array de ints de instancia
+    //declaro un array de ints de instancia
     int[] listaNumeros = {200, 31, 27, 2, 5, 99};
-    int[] listaNumeros2 = {20, 310, 27, 2, 500, 99};
-    int[] listaNumeros3 = {20, 31, 27, 2, 50, 990};
+    int[] listaNumeros2 = {-11, 5, -34, 7, 209, 33, 77, 7};
+    int[] listaNumeros3 = {0, 2000, -4, 1581, 5, 999, 777, 9};
+    int[] listaNumeros4 = {-5, -5, -200, -10, -60, -400};
 
     /*
-    el metodo maximos va aclacular el máximo y el segndo máximo de una lista de números
+    el método máximos va a calcular el máximo y el segundo máximo de una lista de números
      */
     private int[] maximos(int[] lista) {
         //declaramos un array de dos elementos para guardar el máximo y el segundo máximo
         int[] listaMaximos = {lista[0], lista[1]};
 
-        for (int i = 0; i < lista.length; i++) {
-            if (listaMaximos[0] <= lista[i]) {
-                if (i != 0) { //desplazo sólo a la derecha si no estoy justo en la primera posición
-                    listaMaximos[1] = listaMaximos[0]; //desplazo a la derecha el que había como maximo
-                }
-                listaMaximos[0] = lista[i];
-            } else if (listaMaximos[1] <= lista[i]) {//compruebo si el número que estoy leyendo es mayor
-                listaMaximos[1] = lista[i]; //pongo en la SEGUNDA posición al nuevo SEGUNDO máximo
+        //empiezo a comparar desde la posicion 1, no desde la 0
+        for (int i = 1; i < lista.length; i++) {
+            if (lista[i] >= listaMaximos[0]) {
+                //si llega aquí, es que el número que estoy 
+                //comparando es mayor o igual que el que tengo primero en la lista de máximos
+                listaMaximos[1] = listaMaximos[0]; //desplazo a la derecha el que había como máximo
+                listaMaximos[0] = lista[i];  //pongo en la primera posición al nuevo máximo
+            } else if (lista[i] >= listaMaximos[1]) { //compruebo si el número que estoy leyendo es mayor que                                                      //el SEGUNDO de la lista de máximos
+                listaMaximos[1] = lista[i];  //pongo en la SEGUNDA posición al nuevo SEGUNDO máximo
             }
         }
-
         return listaMaximos;
     }
 
     private boolean palindromo(String cadena) {
-        //primera fase: creo un nuevo String que sea una copia del
-        //que me pasan pero quitandole los espacios en blanco
+        //primera fase: creo un nuevo String que sea una copia del 
+        //que me pasan pero quitándole los espacios en blanco
         String auxiliar = "";
         for (int i = 0; i < cadena.length(); i++) {
-            if (cadena.charAt(i) != cadena.charAt(0)) {
+            if (cadena.charAt(i) != ' ') {
                 auxiliar = auxiliar + cadena.charAt(i);
             }
-
         }
-        //ahora en auxliar tengo el string pero sin espacios en blanco
+
+        //ahora en auxiliar tengo el string pero sin espacios en blanco
         //declaro dos indices para que digan qué posiciones estoy comparando
         int indiceIzq = 0;
         int indiceDer = auxiliar.length() - 1;
 
         //mientras sean iguales los caracteres en esas posiciones la palabra será un palindromo
+        //en el momento en el que una de esas comparaciones falle, es que no es palíndromo
         //además, si el indice izquierdo es mayor que el derecho, ya he chequeado toda la frase
         while (auxiliar.charAt(indiceIzq) == auxiliar.charAt(indiceDer) && indiceIzq <= indiceDer) {
             indiceIzq++;
             indiceDer--;
         }
-        boolean resultado = false;
-        if (indiceIzq < indiceDer) {// si esto se cumple es que la palabra no es un palindromo
-            resultado = true;
+        boolean resultado = true;
+        if (indiceIzq < indiceDer) { // si esto se cumple es que la palabra no es un palíndromo
+            resultado = false;
+            System.out.println("NO ES UN PALINDROMO ");
+        } else {
+            System.out.println("SI ES UN PALINDROMO ");
         }
-
-        return true;
+        return resultado;
     }
 
     private void palindromoV2(String cadena) {
@@ -75,22 +79,24 @@ public class EjemplosConsola {
                 auxiliar = auxiliar + cadena.charAt(i);
             }
         }
-        //aqui ya tengo en el string auxiliar todas las letras
+        //aquí ya tengo en el string auxiliar todas las letras
         //de la palabra original pero sin espacios en blanco
 
         int indiceIzq = 0;
         int indiceDer = auxiliar.length() - 1;
 
-        while (auxiliar.charAt(indiceIzq) == auxiliar.charAt(indiceDer) && (indiceIzq <= indiceDer)) {
+        while (auxiliar.charAt(indiceIzq) == auxiliar.charAt(indiceDer)
+                && indiceIzq <= indiceDer) {
             indiceIzq++;
             indiceDer--;
         }
-        boolean resultado = true;
+
         if (indiceIzq < indiceDer) {
-            System.out.print("La cadena " + cadena + "NO es un Palíndromo");
+            System.out.println("La cadena " + cadena + " NO es un Palíndromo");
         } else {
-            System.out.print("La cadena " + cadena + "SI es un Palíndromo");
+            System.out.println("La cadena " + cadena + " SI es un Palíndromo");
         }
+
     }
 
     private boolean esIsograma(String palabra) {
@@ -106,28 +112,70 @@ public class EjemplosConsola {
         return true;
     }
 
-    private void imprimeMes(int num) {
+    private void imprimeMes(int numx) {
+        numx = numx % 7;
+        int contador = 0;
+        //pintara tantas xx como numX sea
 
+        for (int j = 1; j < numx; j++) {
+            System.out.print("XX ");
+            contador = contador + 1;
+        }
         for (int i = 1; i <= 31; i++) {
+            if (contador <= 7) {
+                if (i <= 9) {
+                    System.out.print("0" + i);
+                    System.out.print(" ");
+                }
+            }
+            if (i > 9) {
+                System.out.print(i);
+                System.out.print(" ");
+            }
+            contador = contador + 1;
 
-            if (i < 10) {
-                System.out.print(" " + 0 + i);
-            }
-            if (i >= 10) {
-                System.out.print(" " + i);
-            }
-            if (i % 7 == 0) {
-                System.out.println(" ");
+            if (contador == 7) {
+                System.out.println("");
+                contador = 0;
             }
         }
+        for (int x = contador; x < 7; x++) {
+            System.out.print("XX ");
+        }
 
+    }
+
+    private boolean esAnagrama(String palabraA, String palabraB) {
+        //pasamos a mayúsculas todas las letras
+        palabraA = palabraA.toUpperCase();
+        palabraB = palabraB.toUpperCase();
+
+        boolean anagrama = false; //indica si las palabras son anagramas o no
+        if (palabraA.length() == palabraB.length()) {//solo va chequear si las dos
+            // palabras son igual de grandes
+            for (int i = 0; i < palabraA.length(); i++) {
+                int posicion = 0;
+                while (posicion < palabraB.length() && palabraA.charAt(i) != palabraB.charAt(posicion)  ){
+                    posicion++;   
+                }
+                if (posicion == palabraB.length()){
+                    return false;
+                }
+                else{
+                    palabraB = palabraB.substring(0,posicion) + palabraB.substring(posicion+1);
+                }
+            }
+            if (palabraB.length() == 0){
+                return true;
+            }
+        }
+        return anagrama;
     }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        EjemplosConsola ramon = new EjemplosConsola();
         EjemplosConsola ejercicios = new EjemplosConsola();
 
 //        System.out.println( Arrays.toString( ramon.maximos(ramon.listaNumeros)) );
@@ -139,22 +187,24 @@ public class EjemplosConsola {
 //        ramon.palindromo("ACdfgfgddfgASO HUBO BUHOS ACA") ;
 //        ramon.palindromo("TACOCAT") ;
 //        ramon.palindromo("TACCAT") ;
-//      ramon.palindromoV2("ACASO HUBO BUHOS ACA") ;
-//      ramon.palindromoV2("ACdfgfgddfgASO HUBO BUHOS ACA") ;
-//      ramon.palindromoV2("TACOCAT") ;
-//      ramon.palindromoV2("TACCAT") ;
-//      ejercicios.palindromoV2("ACASO HUBO BUHOS ACA") ;
-//      ejercicios.palindromoV2("ACdfgfgddfgASO HUBO BUHOS ACA") ;
-//      ejercicios.palindromoV2("TACOCAT") ;
-//      ejercicios.palindromoV2("TACCAT") ;
-        //System.out.println("TACCAT  " + ejercicios.esIsograma("TACCAT"));
-        //System.out.println("murcielago  " + ejercicios.esIsograma("murcielago"));
-        //System.out.println("murcielagoo  " + ejercicios.esIsograma("murcielagoo"));
-        for (int i = 0; i < 7; i++) {
-            ejercicios.imprimeMes(i);
-            System.out.println();
-            System.out.println();
-        }
+//        ejercicios.palindromoV2("ACASO HUBO BUHOS ACA") ;
+//        ejercicios.palindromoV2("ACdfgfgddfgASO HUBO BUHOS ACA") ;
+//        ejercicios.palindromoV2("TACOCAT") ;
+//        ejercicios.palindromoV2("TACCAT") ;
+//        
+//        System.out.println("TACCAT  " +ejercicios.esIsograma("TACCAT")) ;
+//        System.out.println("murcielago  " +ejercicios.esIsograma("murcielago")) ;
+//        System.out.println("murcielagoo  " +ejercicios.esIsograma("murcielagoo")) ;
+//        
+        //for (int i = 1; i < 7; i++) {
+            //ejercicios.imprimeMes(i);
+            //System.out.println();
+           //System.out.println();
+        //}
+    
+        System.out.println( "roma amor " + ejercicios.esAnagrama("roma","amor")); 
+        System.out.println( "roma amor " + ejercicios.esAnagrama("roma","pepee"));
     }
-
 }
+
+
